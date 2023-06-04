73df1ac2e0a6c3e25f764e419f565eb3fb42df2f
Windypineda <132728019+Windypineda@users.noreply.github.com>
2023-06-04 05:40:23 +0200

    tamaño video

--- maestro/src/pages/home/index.js
+++ maestro/src/pages/home/index.js
@@ -4,9 +4,10 @@ import Layout from "@/layout/layout";
 export default function Home() {
   return (
       <Layout>
-        <div className="">
-          <video loop autoPlay muted height="50"><source src="/videos/pokemon.mp4"></source></video>
+        <div className="max-w-full flex justify-center p-2" >
+          <video loop autoPlay muted height="70%" width="70%"><source src="/videos/pokemon.mp4"></source></video>
         </div>
       </Layout>
   );
+
 }
