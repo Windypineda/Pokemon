0217cfde22e6faf09d440ba9ca6e0aa7b0ea5413
Carlos Reyes <carlosbisa830@gmail.com>
2023-06-04 15:06:52 -0500

    json pokemon

--- maestro/src/pages/categorias/index.js
+++ maestro/src/pages/categorias/index.js
@@ -1,10 +1,45 @@
 import { useState, useEffect } from "react";
 import Layout from "@/layout/layout";
+import { Carousel } from "flowbite-react";
+import { ProductoCard } from "./components/productoCard";
 
-export default function Categorias(){
-  return(
+export default function Categorias() {
+  const [productos, actualizarProductos] = useState(null);
+  const [alerta, actualizarAlerta] = useState(false);
+  useEffect(() => {
+    async function llamarProductos() {
+      const solicitud = await fetch("./pokemon.json");
+      const respuesta = await solicitud.json();
+      actualizarProductos(respuesta.productos);
+    }
+
+    llamarProductos();
+  }, []);
+
+  return (
     <Layout>
-      <h1>Categorias</h1>
+
+      <div class="titulos">
+        Categorias
+      </div>
+
+      <div>
+        <div className="flex flex-wrap justify-around">
+          {productos ? (
+            productos.map((producto, index) => {
+              return (
+                <ProductoCard
+                  key={index}
+                  producto={producto}
+                ></ProductoCard>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+
     </Layout>
   );
 }
