b19bcd8120bcbb715fcf06d17e7b08b7a4eb8725
Carlos Reyes <carlosbisa830@gmail.com>
2023-06-04 18:53:34 -0500

    json videos

--- maestro/src/pages/categorias/index.js
+++ maestro/src/pages/categorias/index.js
@@ -13,7 +13,6 @@ export default function Categorias() {
       const respuesta = await solicitud.json();
       actualizarProductos(respuesta.productos);
     }
-
     llamarProductos();
   }, []);
 
