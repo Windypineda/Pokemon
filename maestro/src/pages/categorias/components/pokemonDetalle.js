91a9b60b89a8c3c6b78bb5183bc58029023be143
Carlos Reyes <carlosbisa830@gmail.com>
2023-06-04 21:02:57 -0500

    categorias v f

--- maestro/src/pages/categorias/components/pokemonDetalle.js
+++ maestro/src/pages/categorias/components/pokemonDetalle.js
@@ -1,10 +1,13 @@
 import { Button, Card } from "flowbite-react";
 
-export function PokemonDetalle({ producto, volver }) {
+export function PokemonDetalle({ producto, volver,titulo  }) {
   return (
     <>
+         
       <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="" />
+       
+      
+       <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt="" />
         <div class="flex flex-col justify-between p-4 leading-normal">
           <p class="colorletranegro mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-whit">Numero {producto.id}</p>
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{producto.nombre}</h5>
@@ -15,7 +18,7 @@ export function PokemonDetalle({ producto, volver }) {
           <div class="columna">
 
           </div>
-
+          {titulo}
           <table class="table-auto">
 
             <tbody>
