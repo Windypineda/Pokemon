e0000af19196b800ae91849cc971d8c45b8f4700
Carlos Reyes <carlosbisa830@gmail.com>
2023-06-04 21:43:47 -0500

    version 0.6 videos

--- maestro/src/pages/tvPokemon/components/productoCard.js
+++ maestro/src/pages/tvPokemon/components/productoCard.js
@@ -5,15 +5,15 @@ export function ProductoCard({ producto, agregarAlCarrito }) {
 
     <Card className="fondovideos w-200 my-100">
 
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <h5 className="fondovideos text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         <p>{producto.titulo}</p>
       </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
+      <p className=" fondovideos font-normal text-gray-700 dark:text-gray-400">
         <p>{producto.descripcion}</p>
       </p>
       <img  src={producto.imagen}></img>
       <a href={producto.video} >
-        <div class="colorletranegro">
+        <div class="subrallado">
           Ver Video
         </div> </a>
     </Card>
