880f957331e38867dab0d04840ecf4b90658eaaa
Carlos Reyes <carlosbisa830@gmail.com>
2023-06-04 19:32:38 -0500

    videos v1

--- maestro/src/pages/tvPokemon/components/productoCard.js
+++ maestro/src/pages/tvPokemon/components/productoCard.js
@@ -2,16 +2,20 @@ import { Button, Card } from "flowbite-react";
 
 export function ProductoCard({ producto, agregarAlCarrito }) {
   return (
-    
+
     <Card className="w-52 my-5">
-      
+
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
         <p>{producto.titulo}</p>
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         <p>{producto.descripcion}</p>
       </p>
-      <img className="w-52" src={producto.video}></img>
+      <img className="w-52" src={producto.imagen}></img>
+      <a href={producto.video} >
+        <div class="colorletranegro">
+          Ver Video
+        </div> </a>
     </Card>
   );
 }
